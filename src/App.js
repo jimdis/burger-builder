f2728641d9b05d7cff0723ca62508dd4ef1582eb
Jim Disenstam <jim.disenstam@gmail.com>
2019-05-30 12:56:57 +0200

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 const App = () => {
   return (
     <div>
       <Layout>
-        <p>Testing!</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   )
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Aux from '../../hoc/Aux'
+
+const BurgerBuilder = props => {
+  return (
+    <Aux>
+      <div>Burger</div>
+      <div>Build Controls</div>
+    </Aux>
+  )
+}
+
+export default BurgerBuilder
