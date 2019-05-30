9476afff088336d84c0d3b4c59385aac45c8b294
Jim Disenstam <jim.disenstam@gmail.com>
2019-05-30 12:32:55 +0200

    Addin initial structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Layout from './components/Layout/Layout'
 
-function App() {
+const App = () => {
   return (
     <div>
+      <Layout>
+        <p>Testing!</p>
+      </Layout>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
